18754308689fdbaee27f1b1a9b477d68919a03a8
sohebkh13 <sohebkhan10@gmail.com>
2025-01-31 11:25:26 +0530

    all assignments

--- API_Keys.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-from pandasai import Agent
-
-# By default, unless you choose a different LLM, it will use BambooLLM.
-# You can get your free API key signing up at https://pandabi.ai (you can also configure it in your .env file)
-PANDASAI_API_KEY= "$2a$10$fZ4O5yqoPdd5EkVDdIkHmu6l3vyrE/1MtJ1cXjmItuCKU6hjeyeT2"
-
